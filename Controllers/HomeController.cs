b55595268beb9697e63e7b6cfab43b15f5f232b9
Gustavo Carrillo <g2c100@hotmail.com>
2020-08-21 08:38:10 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageFeatures.Models;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+      public ViewResult Index()
+        {
+            var products = new[]
+            {
+               new { Name = "Kayak", Price = 275M},
+
+               new { Name = "Lifejacket", Price = 48.95M},
+
+               new { Name = "Soccer ball", Price = 19.50M},
+
+               new { Name = "Corner flag", Price = 34.95M}
+            };
+            return View(products.Select(p => $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
+        }
+    }
+}
+
---
+++ Models/MyExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> products)
+        {
+            decimal total = 0;
+            foreach(Product prod in products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+        }
+        public static IEnumerable<Product> Filter(this IEnumerable<Product> productEnum, Func<Product, bool> selector)
+        {
+            foreach(Product prod in productEnum)
+            {
+                if (selector(prod))
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+}
